c8737123f66210b3d2f9bea29436bd99180cfe0b
Pietro Pustina <39011004+piepustina@users.noreply.github.com>
2023-12-22 14:49:36 +0100

    Updated configurations

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,8 @@ extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
 
 # Configure the matlab extension
 primary_domain = "mat"
