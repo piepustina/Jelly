20d16944203fc6a0691b0cbbd32e1356a61734e7
Pietro Pustina <39011004+piepustina@users.noreply.github.com>
2024-01-20 18:49:01 +0100

    Update test_simulink.m

--- src/test/test_simulink.m
+++ src/test/test_simulink.m
@@ -1,13 +1,13 @@
 %% Create an instance of the class
 clear; clc;
-L0              = 0.2;
+L0              = 1;
 BaseRadius      = 0.01;
 TipRadius       = 0.01;
 MassDensity     = 1062;
 YoungModulus    = 6.66e5;
 PoissonRatio    = 0.5;
 MaterialDamping = 0.1;
-NGaussPoints    = 10;
+NGaussPoints    = 5;
 n               = 1;
 N_B             = 3;
 Parameters      = [L0, BaseRadius, TipRadius, MassDensity, YoungModulus, PoissonRatio, MaterialDamping]';
@@ -16,15 +16,27 @@ j1              = FixedJoint();
 B1 = cell(N_B, 1);
 J1 = cell(N_B, 1);
 for i = 1:N_B
-    B1{i} = PCC2D([Parameters; NGaussPoints]);
+    %B1{i} = PCC2D([Parameters; NGaussPoints]);
+    B1{i} = PCC2DElongation([Parameters; NGaussPoints]);
     J1{i} = FixedJoint();
 end
 
-% Create the actuators
-A1 = cell(N_B, 1);
+%Create the actuators
+A2 = cell(N_B, 1);
 for i = 1:N_B
-    Parameters  = [(i-1)*L0; i*L0; NGaussPoints; BaseRadius/2; 0; 0];
-    A1{i}       = ConstantDistanceActuator(Parameters);
+   Parameters  = [(i-1)*L0; i*L0; NGaussPoints; BaseRadius/2; 0; 0];
+   A2{i}       = ConstantDistanceActuator(Parameters);
+end
+
+A1 = cell(2, 1);
+for i = 1:2
+   if ~mod(i, 2)
+       signA = -1;
+   else
+       signA = 1;
+   end
+   Parameters  = [0; N_B*L0; NGaussPoints; signA*BaseRadius/2; 0; 0];
+   A1{i}       = ConstantDistanceActuator(Parameters);
 end
 
 
@@ -32,6 +44,8 @@ end
 %r1 = BodyTree(J1, B1);
 
 r1 = SoftRobot(J1, B1, A1);
+
+r2 = SoftRobot(J1, B1, A2);
 
 %% Open the simulink system
 open("simulink_test.slx")
