3b199af48a7aae9d858dd06e3321840da892cf29
Pietro Pustina <39011004+piepustina@users.noreply.github.com>
2024-06-04 17:13:08 +0200

    Update skew4_inv.m

--- src/utils/skew4_inv.m
+++ src/utils/skew4_inv.m
@@ -1,5 +1,22 @@
 function v = skew4_inv(M)
 %SKEW_INV Inverse skew symmetric operator for 4x4 matrices
-v = [M(3,2); M(1,3); M(2,1); M(1:3, 4)];
+
+arguments (Input)
+    M (4, 4, :)
+end
+
+arguments (Output)
+    v (6, :)
+end
+
+% Compute the number of matrices
+nM = size(M, 3);
+
+% Asssign the output
+v = [M(3, 2, 1:nM); ...
+     M(1, 3, 1:nM); ...
+     M(2, 1, 1:nM); ...
+     M(1:3, 4, 1:nM)];
+
 end
 
